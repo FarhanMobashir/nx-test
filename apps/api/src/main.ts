2db59a6232b96b6517cc1665e74f1eacb83b8ae4
Mobashir Farhan <farhanmobashir007@gmail.com>
2022-06-19 02:22:14 +0530

    minor matrix change in actions

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,7 @@ import * as express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api! ▶️' });
+  res.send({ message: 'ping....' });
 });
 
 const port = process.env.port || 3333;
